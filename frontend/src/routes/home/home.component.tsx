294d75688f57706b21f5a8df6dc896c459640b89
Tim Bedsole <23228051+Timothyb92@users.noreply.github.com>
2025-05-08 14:25:15 -0400

    WIP - Commit turned out to be a bigger lift than anticipated. Added JWT functionality, verification middleware

--- frontend/src/routes/home/home.component.tsx
+++ frontend/src/routes/home/home.component.tsx
@@ -17,6 +17,10 @@ export const Home = ({
       <a href="https://discord.com/oauth2/authorize?client_id=1351311738284277800&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fauth%2Fdiscord%2Fcallback&scope=identify+guilds+email">
         Login
       </a>
+      <br />
+      <a href="https://discord.com/oauth2/authorize?client_id=1351311738284277800&response_type=code&redirect_uri=http%3A%2F%2F192.168.5.230%3A8000%2Fauth%2Fdiscord%2Fcallback&scope=identify+email+guilds">
+        LAN IP login
+      </a>
       <button onClick={createMatchListener}>Create match</button>
       <div id="matches"></div>
       <br />
